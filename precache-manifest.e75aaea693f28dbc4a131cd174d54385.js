ac43179f5455b4b85b046c7fc182918a6fa3f4a2
alamgir.inu96 <alamgir.inu96@gmail.com>
2023-01-05 23:43:38 +0500

    Updates

---
+++ precache-manifest.e75aaea693f28dbc4a131cd174d54385.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b4ab1adc37dc52a9199",
+    "url": "/robofriends/static/css/main.56abaac7.chunk.css"
+  },
+  {
+    "revision": "8b4ab1adc37dc52a9199",
+    "url": "/robofriends/static/js/main.8b4ab1ad.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/robofriends/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "bf9ac2dba76b5eec65c3",
+    "url": "/robofriends/static/css/2.4a2eb4b4.chunk.css"
+  },
+  {
+    "revision": "bf9ac2dba76b5eec65c3",
+    "url": "/robofriends/static/js/2.bf9ac2db.chunk.js"
+  },
+  {
+    "revision": "d9a2b70453096add567b",
+    "url": "/robofriends/static/js/runtime~main.d9a2b704.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "306497158691a9dedc13db6b8d8ce273",
+    "url": "/robofriends/index.html"
+  }
+];
